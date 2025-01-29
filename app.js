00091a101a339c22e4d6065f19f23914ea0a7650
MaramLA <maramalfaraj2000@gmail.com>
2025-01-29 23:11:04 +0300

    editing responses

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import { createNote, getNote, getNotes } from "./database.js";
-
+import cors from "cors";
 // loading environment variables
 dotenv.config();
 
@@ -10,14 +10,15 @@ const app = express();
 
 const port = process.env.PORT;
 
-// middleware
+// middlewares
+app.use(cors());
 app.use(express.json());
 
 // routes
 app.get("/notes", async (req, res, next) => {
   try {
     const notes = await getNotes();
-    res.status(200).send(notes);
+    res.status(200).json(notes);
   } catch (error) {
     next(error);
   }
@@ -27,7 +28,7 @@ app.get("/notes/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const note = await getNote(id);
-    res.status(200).send(note);
+    res.status(200).json(note);
   } catch (error) {
     next(error);
   }
@@ -38,7 +39,7 @@ app.post("/notes", async (req, res, next) => {
     console.log(req.body);
     const { title, contents } = req.body;
     const newNote = await createNote(title, contents);
-    res.status(201).send(newNote);
+    res.status(201).json(newNote);
   } catch (error) {
     next(error);
   }
@@ -47,7 +48,7 @@ app.post("/notes", async (req, res, next) => {
 // error handling
 app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).send("something went wrong");
+  res.status(500).json("something went wrong");
 });
 
 // runnining server
